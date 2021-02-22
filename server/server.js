71889fbb6dd07417edaa1bf3b2ee666c8717be4d
chagy <chagy_x_@hotmail.com>
2021-02-22 11:39:15 +0700

    Server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config() 
+
+const app = express()
+
+mongoose
+    .connect(process.env.DATABASE,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,  
+    })
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log("DB CONNECTION ERR",err));
+
+app.use(morgan("dev"))
+app.use(bodyParser.json({limit: "2mb"}));
+app.use(cors());
+
+app.get("/api",(req,res) => {
+    res.json({
+        data:"hey you API"
+    })
+})
+
+const port = process.env.PORT || 8000;
+
+app.listen(port,() => console.log(`Server is running on port ${port}`));
